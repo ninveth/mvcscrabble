dcd7b8e475b21c7142a1963b99e75b05ad488810
cr701979 <cr701979@73fd81bf-7de4-558b-50e4-a69c11297be8>
2010-11-16 22:43:42 +0000

    Controler

--- src/fr/scrabble/controler/Controler.java
+++ src/fr/scrabble/controler/Controler.java
@@ -1,5 +1,50 @@
-package fr.scrabble.controler;
+package src.fr.scrabble.controler;
 
-public class Controler {
+import src.fr.scrabble.modele.modele.AbstractModel;
+import src.fr.scrabble.modele.modele.Word;
+
+
+
+public class Controler implements ControlerInterface {
+    
+ private AbstractModel model; 
+    
+    public Controler(AbstractModel model){
+        this.model = model;
+    }
+
+    @Override
+    public AbstractModel getModel() {
+        return this.model;
+    }
+
+    @Override
+    public boolean lookup(String mot) {
+         return this.model.lookup(mot);
+        
+    }
+
+    @Override
+    public void newGame(String p1name, String p2name) {
+     this.model.newGame(p1name,p2name);
+        
+    }
+
+    @Override
+    public void poserMot(String word, int i, int j, char direction) {
+       
+            Word newWord = new Word(word, i, j, direction);
+            this.model.addWord(newWord);
+        
+    }
+    public void skip(){
+        this.model.skip();
+    }
+
+    @Override
+    public void setEndGameCount() {
+        this.model.setEndGameCount();
+        
+    }
 
 }
