2ffcd53077bdd34b66bcf4d9b8aee0edf4201f7c
cr701979 <cr701979@73fd81bf-7de4-558b-50e4-a69c11297be8>
2010-11-20 18:32:34 +0000

    class BagIsEmptyException

---
+++ src/fr/scrabble/modele/exceptions/BagIsEmptyException.java
@@ -0,0 +1,12 @@
+package src.fr.scrabble.modele.exceptions;
+
+@SuppressWarnings("serial")
+public class BagIsEmptyException extends RuntimeException {
+    
+    
+    public BagIsEmptyException() {
+        super(); 
+
+    }
+
+}
